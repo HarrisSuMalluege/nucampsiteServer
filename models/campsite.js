71394b025580cf37122ebb55f615cf877fdd53c6
“Harris <harris.su.malluege@gmail.com>
2020-07-10 12:32:50 -0500

    Express REST API with MongoDB and Mongoose Part 1

---
+++ models/campsite.js
@@ -0,0 +1,73 @@
+/*
+2020-07-09
+This is to defined campsite's schemas and the model for all documents in our databases campsites collection
+*/
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; // Making a shorthand instead of mongoose.Schema.
+
+// To require mongoose-currency from the library
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+
+
+// Creating comment Schema (subdocuments).
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        require: true
+    },
+    text: {
+        type: String,
+        require: true
+    },
+    author: {
+        type: String,
+        require: true
+    }
+},{
+    timestamps: true
+});
+
+// Creating a Schema, and include two agruments, the first argument is required.
+const campsiteSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    }, 
+    image: {
+        type: String,
+        required: true
+    },
+    elevation: {
+        type: Number,
+        required: true
+    },
+    cost: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+    // Adding array of subdocuments to campsite document.
+    comments: [commentSchema]
+}, {
+    timestamps: true
+});
+
+// Using the Schema to create a campsite model.
+const Campsite = mongoose.model("Campsite", campsiteSchema);
+
+// exporting the model
+module.exports = Campsite;
--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,26 +1,44 @@
+/*
+2020-07-10
+This file is to handle get, put , post and delete (CRUD) endpoints
+*/
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const Campsite = require('../models/campsite');
+const { response } = require('express');
 
 const campsiteRouter = express.Router();
 
 campsiteRouter.use(bodyParser.json());
 
 campsiteRouter.route('/')
-.all((req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  next(); // the next function is passed control of the application routing to the next relevant routing method after this one, otherwise it would just stop here and not go any further
-})
+// .all((req, res, next) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   next(); // the next function is passed control of the application routing to the next relevant routing method after this one, otherwise it would just stop here and not go any further
+// })
 
-.get((req, res) => {
-  res.end("Will send all the campsites to you");
+.get((req, res, next) => {
+  Campsite.find()
+  .then(campsites => {
+    res.statusCode = 200;
+    res.setHeader ('Content-Type', 'application/json');
+    res.json(campsites);
+  })
+  .catch(err => next(err));
 })
 
 // Use body-parser to handle user create request
-.post((req, res) => {
-  res.end(
-    `Will add the campsite: ${req.body.name} with description: ${req.body.description}`
-  );
+.post((req, res, next) => {
+  Campsite.create(req.body)
+  .then(campsite => {
+    console.log('Campsite Created ', campsite);
+    res.statusCode = 200,
+    res.setHeader('Content-Type', 'application/json');
+    res.json(campsite);
+  })
+  .catch(err => next(err));
 })
 
 // To handle user updates
@@ -30,14 +48,26 @@ campsiteRouter.route('/')
 })
 
 // To delete user request
-.delete((req, res) => {
-  res.end('Deleting all campsites');
+.delete((req, res, next) => {
+  Campsite.deleteMany()
+  .then(response => {
+    res.statusCode = 200,
+    res.setHeader('Content-Type', 'application/json');
+    res.json(response);
+  })
+  .catch(err => next(err));
 });
 
 // Workshop assignment task 1
 campsiteRouter.route('/:campsiteId')
-.get((req, res) => {
-  res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+.get((req, res, next) => {
+  Campsite.findById(req.params.campsiteId)
+  .then(campsite => {
+    res.statusCode = 200,
+    res.setHeader('Content-Type', 'application/json');
+    res.json(campsite);
+  })
+  .catch(err => next(err));
 })
 
 // Use body-parser to handle user create request
@@ -47,15 +77,27 @@ campsiteRouter.route('/:campsiteId')
 })
 
 // To handle user updates
-.put((req, res) => {
-  res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-  res.end(`Will update the campsite: ${req.body.name}
-    with description: ${req.body.description}`);
+.put((req, res, next) => {
+  Campsite.findByIdAndUpdate(req.params.campsiteId, {
+    $set: req.body },
+    { new: true })
+    .then(campsite => {
+      res.statusCode =200,
+      res.setHeader('Content-Type', 'application/json');
+      res.json(campsite);
+    })
+    .catch(err => next(err));
 })
 
 // To delete user request
 .delete((req, res) => {
-  res.end(`Deleteing campsite: ${req.params.campsiteId}`);
+  Campsite.findByIdAndDelete(req.params.campsiteId)
+  .then(response => {
+    res.statusCode = 200,
+    res.setHeader('Content-Type', 'application/json');
+    res.json(response);
+  })
+  .catch(err => next(err));
 });
 
 
